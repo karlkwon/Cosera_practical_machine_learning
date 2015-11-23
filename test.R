d67b6588788e8e9c68efb43610882ba6504ed1ce
mrthink <rstudio@example.com>
2015-11-23 00:49:44 +0000

    ...

--- test.R
+++ test.R
@@ -13,27 +13,17 @@ training_tf <- ifelse(is.na(training), 1, 0)
 training_tf_cml <- colSums(training_tf)
 training_tf_cml_nor <- training_tf_cml / dim(training_tf)[1]
 
-## I only care the variable, which has less than 0.1 ratio.
-training2 <- training[,training_tf_cml_nor < 0.1]
+## I only care the variable, which has less than 0.1 NA ratio.
+hist(training_tf_cml_nor)
+validColumns <- training_tf_cml_nor < 0.1
+training2 <- training[, validColumns]
 
 dim(training2)
 
-dim(training)
-training2_tf <- ifelse(is.finite(training2), 1, 0)
+## and i ignore the variable which has meaningless data.
+training3 <- training2[,8:60]
+dim(training3)
 
-
-
-hist(training_tf_cml_nor)
-## so i drop the data...
-
-
-training_tf <- training[is.na(training)]
-
-dim(is.na(training))
-is.data.table(training[is.na(training)])
-
-head(training_tf)
-colSums(training_tf)
 
 
 
